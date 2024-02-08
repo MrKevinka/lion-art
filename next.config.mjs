1bb99dc484b17d055499c53499567e0e470f2614
Markus Renken <z0n@users.noreply.github.com>
2024-02-08 12:11:26 +0100

    chore: make next lint run everywhere

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 import BundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    dirs: ['.'],
+  },
+};
 
 const withBundleAnalyzer = BundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
