b77026fd7a3d60ae55a0372233b91096109c28e8
Markus Renken <z0n@users.noreply.github.com>
2024-05-17 11:08:59 +0200

    chore: update packages

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,7 +3,6 @@ import '../src/app/globals.css';
 import type { Preview } from '@storybook/react';
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
